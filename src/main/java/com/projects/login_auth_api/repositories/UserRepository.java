575541dbca896c36dd793bd578c77d87cb4ec90e
Mauro-vidal <mauroviidal@gmail.com>
2024-11-19 14:33:22 -0300

    SSepring Security

--- src/main/java/com/projects/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/projects/login_auth_api/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.projects.login_auth_api.repositories;
 import com.projects.login_auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
