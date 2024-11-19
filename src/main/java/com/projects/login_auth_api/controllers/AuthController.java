cbb5011f30a45974139da3a7b60ea6ed116361e7
Mauro-vidal <mauroviidal@gmail.com>
2024-11-19 15:02:18 -0300

    dtos e controller

---
+++ src/main/java/com/projects/login_auth_api/controllers/AuthController.java
@@ -0,0 +1,55 @@
+package com.projects.login_auth_api.controllers;
+
+import com.projects.login_auth_api.domain.user.User;
+import com.projects.login_auth_api.dto.LoginRequestDTO;
+import com.projects.login_auth_api.dto.RegisterRequestDTO;
+import com.projects.login_auth_api.dto.ResponseDto;
+import com.projects.login_auth_api.infra.security.TokenService;
+import com.projects.login_auth_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body) {
+        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+        if (passwordEncoder.matches(user.getPassword(), body.password())){
+            String token = this.tokenService.generateToken(user);
+            return ResponseEntity.ok(new ResponseDto(user.getId(), token));
+
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body) {
+        Optional<User> user = this.repository.findByEmail(body.email());
+
+        if (user.isPresent()) {
+            User newUser = new User();
+            newUser.setEmail(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            this.repository.save(newUser);
+
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDto(newUser.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
---
+++ src/main/java/com/projects/login_auth_api/dto/ResponseDto.java
@@ -0,0 +1,4 @@
+package com.projects.login_auth_api.dto;
+
+public record ResponseDto(String name, String token) {
+}
