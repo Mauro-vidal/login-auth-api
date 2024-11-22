6ed38e550fb340002ee1fd5da73b52cbba336abe
Mauro-vidal <mauroviidal@gmail.com>
2024-11-21 22:33:36 -0300

    endpoint get para todos os susuários.

--- src/main/java/com/projects/login_auth_api/controllers/UserController.java
+++ src/main/java/com/projects/login_auth_api/controllers/UserController.java
@@ -1,16 +1,32 @@
 package com.projects.login_auth_api.controllers;
 
+import com.projects.login_auth_api.domain.user.User;
+import com.projects.login_auth_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+
+    private final UserRepository repository;
 
     @GetMapping
     public ResponseEntity<String> getUser() {
         return ResponseEntity.ok("sucesso!");
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUser() {
+        List<User> users = repository.findAll();
+        return ResponseEntity.ok(users);
     }
 }
